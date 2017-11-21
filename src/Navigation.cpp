699d45475889e052c287ee587dc1fef2ebbaafd9
senthilarul <sarul1@terpmail.umd.edu>
2017-11-21 14:47:09 -0500

    modified files to add Doxygen comments for classes and methods

--- src/Navigation.cpp
+++ src/Navigation.cpp
@@ -1,6 +1,44 @@
+/**
+ * Copyright (c) 2017, Senthil Hariharan Arul
+ *
+ * Redistribution and use in source and binary forms, with or without  
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the   
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    Navigation.cpp
+ * @author  Senthil Hariharan Arul
+ * @copyright 3-clause BSD
+ * @brief method declaration for Navigation class
+ * Defines the methods for the navigation class.
+ */
+
 #include "Navigation.hpp"
-
-
 
 Navigation::Navigation() {
 
@@ -14,11 +52,12 @@ Navigation::Navigation() {
   msg.angular.x = 0.0;
   msg.angular.y = 0.0;
   msg.angular.z = 0.0;
+  // publish velocity values for turtlebot
   velocity.publish(msg);
 }
 
 Navigation::~Navigation() {
-
+  // stop the robot motion
   msg.linear.x = 0.0;
   msg.linear.y = 0.0;
   msg.linear.z = 0.0;
@@ -29,14 +68,17 @@ Navigation::~Navigation() {
 }
 
 void Navigation::move() {
-  
+  // set loop rate
   ros::Rate loop_rate(10);
   while (ros::ok()) {
     // std::cout<<depth.collisionCheck()<<"\n";
+    // check for obstacle
     if (depth.collisionCheck()) {
+      // stop linear motion and rotate turtlebot to avoid collision 
       msg.linear.x = 0.0;
       msg.angular.z = 1.0;
     } else {
+      // if no obstacle move straight
       msg.linear.x = 0.2;
       msg.angular.z = 0.0;
     }
